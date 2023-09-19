074bdc37a4c0972b1f8cb4a9ec37fce8e3d8fb0e
Lovenchia <145438695+Lovenchia@users.noreply.github.com>
2023-09-19 19:44:47 +0800

    Add files via upload

---
+++ abouttugas/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace abouttugas
+{
+    partial class informatiion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(informatiion));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 308);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "Deskripsi :",
+            "",
+            "Perkenalkan, saya Lovenchia Warouw seorang Mahasiswi Jurusan Informatika yang ber" +
+                "kuliah di Universitas Klabat di Airmadidi, dan saya senang bisa berbagi sedikit " +
+                "tentang diri saya dengan Anda.",
+            "Saya hidup dengan moto \'Hidup adalah petualangan,\' dan dalam halaman ini, saya in" +
+                "gin mengajak Anda menjelajahi perjalanan hidup saya.",
+            "Saya adalah seorang pengendara motor dengan pengalaman lebih dari 7 tahun.",
+            "Pengalaman mengendarai motor adalah salah satu pengalaman yang dapat memberikan k" +
+                "egembiraan, kebebasan, dan tantangan yang unik. ",
+            "Ini adalah sebagian dari cerita saya saat mengendarai motor.",
+            "Sejak saya pertama kali mengayuh motor, saya merasakan kebebasan yang luar biasa." +
+                " Angin yang menyapu wajah dan rambut saya, serta getaran mesin yang bergejolak d" +
+                "i bawah saya, ",
+            "memberikan sensasi yang sulit diungkapkan dengan kata-kata. ",
+            "Saya masih ingat hari pertama saya belajar mengendarai motor, ketika saya harus b" +
+                "erjuang untuk menjaga keseimbangan dan memahami bagaimana merespons setiap putar" +
+                "an gas dan pengereman.",
+            "Seiring berjalannya waktu, pengalaman mengendarai motor menjadi lebih lancar dan " +
+                "nyaman. Namun, ketika mengendarai motor saya selalu merasakan tantangan untuk me" +
+                "laju lebih cepat dan lebih cepat lagi.",
+            resources.GetString("listBox1.Items"),
+            "dapat memberikan saya semangat untuk melambung melampaui mereka.",
+            "di jalanan umum yang merupakan aktivitas banyak kendaraan dan orang-orang yang be" +
+                "rlalu lalang merupakan tempat saya untuk mengalirkan imajinasi saya dalam mengen" +
+                "darai motor.",
+            "Namun seiring berjalannya waktu saya selalu mendapatkan masalah dengan pengendara" +
+                " yang lain serta orang-orang atau hewan peliharaan sekitar yang sering kali berm" +
+                "ain di pinggiran jalan.",
+            resources.GetString("listBox1.Items1"),
+            resources.GetString("listBox1.Items2"),
+            "kita tentang pentingnya menjalani hidup dengan penuh makna dan pencapaian. ",
+            "Hingga tibalah saya di Universitas Klabat untuk meraih seni positif dan pencapaia" +
+                "n yang akan menyenangkan bukan hanya bagi diri saya tapi juga untuk orang tua sa" +
+                "ya. God Bless You All.",
+            ""});
+            this.listBox1.Location = new System.Drawing.Point(231, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(486, 304);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Britannic Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "AboutMe";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(231, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 45);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "View Artikel";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // informatiion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.MistyRose;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HelpButton = true;
+            this.Name = "informatiion";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ abouttugas/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace abouttugas
+{
+    public partial class informatiion : Form
+    {
+        public informatiion()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void information_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
